bda626b9ab01eaaca15c088e8ba8203ebfe38642
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-02 16:13:10 +0330

    Setup ChakraUI and theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { myTheme } from "@/styles/theme.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={myTheme}>
+    <App />
+  </ChakraProvider>
+);
