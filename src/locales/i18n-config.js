84527569b0598f85635a843dde44c036258e44cd
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-03 12:55:42 +0330

    Default language from browser and cache

--- src/locales/i18n-config.js
+++ src/locales/i18n-config.js
@@ -1,19 +1,25 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 import homeEN from "@/locales/en/home.json";
 import homeFR from "@/locales/fr/home.json";
 import homeFA from "@/locales/fa/home.json";
-i18n.use(initReactI18next).init({
-  resources: {
-    fa: {
-      home: homeFA,
+
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      fa: {
+        home: homeFA,
+      },
+      en: {
+        home: homeEN,
+      },
+      fr: {
+        home: homeFR,
+      },
     },
-    en: {
-      home: homeEN,
-    },
-    fr: {
-      home: homeFR,
-    },
-  },
-  fallbackLng: "en",
-});
+    fallbackLng: "en",
+  });
