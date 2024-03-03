d0e975780674eb5b7442666955816629785c975a
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-03 09:55:44 +0330

    Customize a Badge

--- src/features/Landing.jsx
+++ src/features/Landing.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Heading, Text } from "@chakra-ui/react";
+import { Box, Badge, Flex, Image, Heading, Text } from "@chakra-ui/react";
 import humanImg from "@/assets/images/human.png";
 export const Landing = () => {
   const leftSection = (
@@ -22,10 +22,18 @@ export const Landing = () => {
       </Text>
     </Box>
   );
+  const badgeExperience = (
+    <Badge bg={"primary.light"} borderRadius={7} p={3} textAlign={"center"}>
+      <Text fontSize={"x-large"}>{new Date().getFullYear() - 2023}</Text>
+      <Text>years of experience</Text>
+    </Badge>
+  );
+
   const rightSection = (
-    <>
+    <Box mt={{ base: 10, md: 0 }}>
+      <Flex justify={"flex-end"}>{badgeExperience}</Flex>
       <Image src={humanImg} w={400} />
-    </>
+    </Box>
   );
 
   return (
