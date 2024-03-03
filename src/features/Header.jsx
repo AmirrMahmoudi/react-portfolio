4e8858b235346a8fa1989db6feb853b5e5ca061c
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-03 12:34:24 +0330

    Change language

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -1,10 +1,31 @@
-import { Image, Link, Flex, HStack } from "@chakra-ui/react";
+import {
+  Image,
+  Link,
+  Flex,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Button,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+
+import { ChevronDownIcon } from "@chakra-ui/icons";
+
 import logoImg from "@/assets/images/logo.png";
 import bubbleImg from "@/assets/images/bubble.png";
 import flagENImg from "@/assets/images/flag-en.png";
+import flagFRImg from "@/assets/images/flag-fr.png";
+import flagFAImg from "@/assets/images/ir.png";
 import { useTranslation } from "react-i18next";
+
 export const Header = () => {
-  const { t } = useTranslation("home");
+  const { t, i18n } = useTranslation("home");
+  const switchLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <Flex justify={"space-between"}>
       <Image src={logoImg} h={10} />
@@ -17,7 +38,43 @@ export const Header = () => {
         >
           {t("hireMe")}
         </Link>
-        <Image pl={20} src={flagENImg} h={8} />
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            <Image
+              src={
+                i18n.language === "en"
+                  ? flagENImg
+                  : i18n.language === "fr"
+                  ? flagFRImg
+                  : flagFAImg
+              }
+              borderRadius={"50%"}
+              h={8}
+              w={8}
+              cursor={"pointer"}
+            />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => switchLanguage("en")}>
+              <Box display="flex" w={"90%"} justifyContent={"space-between"}>
+                <Image src={flagENImg} h={8} />
+                <Text>English</Text>
+              </Box>
+            </MenuItem>
+            <MenuItem onClick={() => switchLanguage("fa")}>
+              <Box display="flex" w={"90%"} justifyContent={"space-between"}>
+                <Image src={flagFAImg} h={9} w={9} borderRadius={"50%"} />
+                <Text>فارسی</Text>
+              </Box>
+            </MenuItem>
+            <MenuItem onClick={() => switchLanguage("fr")}>
+              <Box display="flex" w={"90%"} justifyContent={"space-between"}>
+                <Image src={flagFRImg} h={8} />
+                <Text>Français</Text>
+              </Box>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
     </Flex>
   );
--- src/locales/i18n-config.js
+++ src/locales/i18n-config.js
@@ -2,8 +2,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import homeEN from "@/locales/en/home.json";
 import homeFR from "@/locales/fr/home.json";
+import homeFA from "@/locales/fa/home.json";
 i18n.use(initReactI18next).init({
   resources: {
+    fa: {
+      home: homeFA,
+    },
     en: {
       home: homeEN,
     },
