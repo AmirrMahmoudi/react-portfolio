cb494740b46c507c5bccd6bffafbc5a1e4540dc6
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-03-04 10:21:16 +0330

    React firebase setup

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-f2KXYcfLQLw4M5J9BvsOXnmx-Bk-A1g",
+  authDomain: "portfolio-l1fij8.firebaseapp.com",
+  projectId: "portfolio-l1fij8",
+  storageBucket: "portfolio-l1fij8.appspot.com",
+  messagingSenderId: "956832611403",
+  appId: "1:956832611403:web:19e704f70379cd73b4e1ce",
+};
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { myTheme } from "@/styles/theme.js";
+import { FirebaseApp } from "./utils/firebase";
+
+FirebaseApp.init();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider theme={myTheme}>
